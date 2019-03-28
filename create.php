443c56139e723f9a050350abf65bc09c011388f8
Ghena-wi <genha.wi@gmail.com>
2019-03-28 06:54:56 +0200

    working create function1

--- create.php
+++ create.php
@@ -41,7 +41,7 @@ if ($conn->query($insr1) === TRUE) {
        <form action="" method="post">
         
         <label>Correspondence Author</label>
-           <input class="form-control" type="email" name="ca">
+           <input class="form-control" type="email" name="ca"  id="caemail">
            <!-- <div> -->
 <!-- <form action="#" method="post"> -->
 <input type="checkbox" name="job_list[]" value="Create journal" id="cj" class="Checkbox"><label>Create journal</label><br/>
@@ -53,21 +53,23 @@ if ($conn->query($insr1) === TRUE) {
 <input type="checkbox" name="job_list[]" value="Selection of reviewers" id="sr" class="Checkbox"><label>Selection of reviewers</label><br/>
 <!-- <input type="submit" name="submit1" value="Submit"/> -->
 <button type="button" id="test" value="click" onclick="loadDoc()">Change Content</button>
+<button type="button" id="reset" value="click" onclick="resetcheckboxes()">Reset</button>
+
 <!-- <input type="button" id="test" value="click"> -->
 <!-- </form></div> -->
 <div id="demo"></div>
 
- <label>Correspondence Author</label>
-           <input class="form-control" type="email" name="ca">
+ <label id="ca">Correspondence Author</label>
+           <input class="ca" type="email" name="ca" id="caemail">
            <!-- <div> -->
 <!-- <form action="#" method="post"> -->
-<input type="checkbox" name="job_list[]" value="Create journal" id="cj1c" class="Checkbox"><label  id="cj1l">Create journal</label><br/>
-<input type="checkbox" name="job_list[]" value="Classification" id="c1c" class="Checkbox"><label id="c1l">Classification</label><br/>
-<input type="checkbox" name="job_list[]" value="Review" id="r1c" class="Checkbox"><label id="r1l">Review</label><br/>
-<input type="checkbox" name="job_list[]" value="Make decision" id="md1c" class="Checkbox"><label id="md1l">Make decision</label><br/>
-<input type="checkbox" name="job_list[]" value="Make reports" id="mr1c" class="Checkbox"><label id="mr1l">Make reports</label><br/>
-<input type="checkbox" name="job_list[]" value="Request reviewers" id="rr1c" class="Checkbox"><label id="rr1l">Request reviewers</label><br/>
-<input type="checkbox" name="job_list[]" value="Selection of reviewers" id="sr1c" class="Checkbox"><label id="sr1l">Selection of reviewers</label><br/>
+<input type="checkbox" name="job_list[]" value="Create journal" id="cj1c" class="Checkbox1"><label  id="cj1l">Create journal</label><br/>
+<input type="checkbox" name="job_list[]" value="Classification" id="c1c" class="Checkbox1"><label id="c1l">Classification</label><br/>
+<input type="checkbox" name="job_list[]" value="Review" id="r1c" class="Checkbox1"><label id="r1l">Review</label><br/>
+<input type="checkbox" name="job_list[]" value="Make decision" id="md1c" class="Checkbox1"><label id="md1l">Make decision</label><br/>
+<input type="checkbox" name="job_list[]" value="Make reports" id="mr1c" class="Checkbox1"><label id="mr1l">Make reports</label><br/>
+<input type="checkbox" name="job_list[]" value="Request reviewers" id="rr1c" class="Checkbox1"><label id="rr1l">Request reviewers</label><br/>
+<input type="checkbox" name="job_list[]" value="Selection of reviewers" id="sr1c" class="Checkbox1"><label id="sr1l">Selection of reviewers</label><br/>
 <!-- <input type="submit" name="submit1" value="Submit"/> -->
 <button type="button" id="test" value="click" onclick="loadDoc1()">Change Content</button>
 <!-- <input type="button" id="test" value="click"> -->
@@ -82,35 +84,52 @@ if ($conn->query($insr1) === TRUE) {
 // });
 
 function loadDoc() {
+    var email1 = document.getElementById("caemail").value ;
+    var role1 = document.getElementById("ca").innerHTML ;
     var func1 = ($('.Checkbox:checked').map(function() {
-    return this.value; }).get().join(', '));
+    return this.value; }).get().join(','));
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       var funn = this.responseText.split(',');
-       for (var i = 0; i < funn.length; i++) { 
-    if(funn[i] ==document.getElementById("cj1l").innerHTML ){ document.getElementById("cj1c").disabled = true;}
-    else if(funn[i] ==document.getElementById("c1l").innerHTML ){ document.getElementById("c1c").disabled = true;}
-
-
-     else if(  document.getElementById("r1l").innerHTML ==  funn[i]){
+        var r=this.responseText;
+        console.log(r);
+       var funn = r.split(',');
+    //    for(var x=0;x<funn.length;x++){
+    //        console.log(funn[x]);}
+       document.getElementById("demo").innerHTML=funn;
+      
+       for(var x=0;x<funn.length;x++){
+        // console.log(funn[x]);
+    if(funn[x] == document.getElementById("cj1l").innerHTML ){ document.getElementById("cj1c").disabled = true;}
+    else if(funn[x] == document.getElementById("c1l").innerHTML ){ document.getElementById("c1c").disabled = true;}
+     else if( funn[x] ==  document.getElementById("r1l").innerHTML   ){
         document.getElementById("r1c").disabled = true;
-     }else if(  document.getElementById("md1l").innerHTML ==  funn[i]){
+     }else if( funn[x] ==  document.getElementById("md1l").innerHTML   ){
         document.getElementById("md1c").disabled = true;
-     }else if(  document.getElementById("mr1l").innerHTML ==  funn[i]){
+     }else if( funn[x] ==  document.getElementById("mr1l").innerHTML){
         document.getElementById("mr1c").disabled = true;
-     }else if(  document.getElementById("rr1l").innerHTML ==  funn[i]){
+     }else if( funn[x] ==  document.getElementById("rr1l").innerHTML   ){
         document.getElementById("rr1c").disabled = true;
-     }else if(  document.getElementById("sr1l").innerHTML ==  funn[i]){
+     }else if( funn[x] ==  document.getElementById("sr1l").innerHTML   ){
         document.getElementById("sr1c").disabled = true;
      }
     } } 
   };
-  xhttp.open("GET", "fun.php?func1="+func1, true);
+  xhttp.open("GET", "fun.php?func1="+func1+"&email1="+email1+"&role1="+role1, true);
   xhttp.send();
 }
 
+function resetcheckboxes() {
+    document.getElementById("cj").checked = true;
+    document.getElementById("c").checked = false;
+    document.getElementById("r").checked = false;
+    document.getElementById("md").checked = false;
+    document.getElementById("mr").checked = true;
+    document.getElementById("rr").checked = true;
+    document.getElementById("sr").checked = false;
+    
+}
 </script>
 
 <!-- 
--- fun.php
+++ fun.php
@@ -1,10 +1,60 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jp";
 
-echo $_GET['func1'];
-// $func1 = $_GET['func1'];
+
 // $func1Array = explode (",", $func1);  
 // foreach ($func1Array as $value) {
-//     echo $value ;
+    // echo $value ;
 // }
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+if(isset($_GET['role1'])){
+    $roleid= "select R_id from roles where R_name = \"".$_GET['role1']."\"";
+    $result=$conn->query($roleid);
+    // }
+    // if ($conn->query($roleid) === TRUE) {
+    //     echo "New record created successfully";
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if(isset($_GET['func1'])&&isset($_GET['email1'])){
+                $insr1= "insert into mr_relation (R_id,email,func) values ('" .  $row['R_id'] . "', '" . $_GET['email1'] ."','" .$_GET['func1'] ."')";
+                
+                }
+                if ($conn->query($insr1) === TRUE) {
+                    ;}
+                    else {
+                        echo "Error: " . $insr1 . "<br>" . mysqli_error($conn);
+                    }
+        }
+    } else {
+        echo "0 results";
+    }
 
+
+// if(isset($_GET['func1'])){
+// $insr1= "insert into mr_relation (R_id,email,func) values ('" .  $roleid . "', '" . $_GET['email1'] ."','" .$_GET['func1'] ."')";
+
+// }
+// if ($conn->query($insr1) === TRUE) {
+//     echo "New record created successfully";}
+//     else {
+//         echo "Error: " . $insr1 . "<br>" . mysqli_error($conn);
+//     }
+    
+//   }else {
+//     echo "Error: " . $roleid . "<br>" .  mysqli_error($conn);
+// }
+ 
+$func1 = $_GET['func1'];
+echo $func1;
+}
 ?>
